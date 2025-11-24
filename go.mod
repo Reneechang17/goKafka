baefb7fc25aeb9622b56dc6639ab8b42c183e2f8
Reneechang17 <minnie90061717@gmail.com>
2025-11-23 21:12:28 -0800

    Add official consumer/producer, WIP for decoding

---
+++ go.mod
@@ -0,0 +1,5 @@
+module goKafka
+
+go 1.25.1
+
+require github.com/confluentinc/confluent-kafka-go/v2 v2.12.0 // indirect
--- main.go
+++ main.go
@@ -1,17 +1,36 @@
 package main
 
 import (
+	"bytes"
+	"encoding/binary"
 	"fmt"
 	"io"
+	"log"
 	"log/slog"
 	"net"
+	"time"
+
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
 )
 
+type Message struct {
+	data []byte
+	// ...
+}
+
 type Server struct {
+	coffsets map[string]int
+	buffer []Message
+
 	ln net.Listener
 }
 
-func NewServer() *Server { return &Server{}}
+func NewServer() *Server { 
+	return &Server{
+		coffsets: make(map[string]int),
+		buffer: make([]Message, 0),
+	}
+}
 
 func (s *Server) Listen() error {
 	ln, err := net.Listen("tcp", ":9092")
@@ -33,9 +52,104 @@ func (s *Server) Listen() error {
 
 func (s *Server) handleConn(conn net.Conn) {
 	fmt.Println("new connection", conn.RemoteAddr())
+
+	buf := make([]byte, 1024)
+	for {
+		n, err := conn.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				return
+			}
+			slog.Error("connection read error", "err", err)
+			return
+		}
+		msg := buf[:n]
+		r := bytes.NewReader(msg)
+
+		acks := make([]byte, 2)
+		binary.Read(r, binary.LittleEndian, acks)
+
+		timeoutms := binary.BigEndian.Uint32(msg[2:6])
+
+		fmt.Println("acks: ", acks)
+		fmt.Println("timeoutms: ", timeoutms)
+	}
 }
 
 func main() {
 	server := NewServer()
-	server.Listen()
+	go func() {
+		log.Fatal(server.Listen())
+	}()
+	time.Sleep(time.Second)
+
+	fmt.Println("producing....")
+
+	consume()
+}
+
+func produce() error{
+	p, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": "localhost:9092"})
+	if err != nil {
+		return err
+	}
+
+	defer p.Close()
+	// Delivery report handler for produced messages
+	go func() {
+		for e := range p.Events() {
+			switch ev := e.(type) {
+			case *kafka.Message:
+				if ev.TopicPartition.Error != nil {
+					fmt.Printf("Delivery failed: %v\n", ev.TopicPartition)
+				} else {
+					fmt.Printf("Delivered message to %v\n", ev.TopicPartition)
+				}
+			}
+		}
+	}()
+
+	// Produce messages to topic (asynchronously)
+	topic := "fooTopic"
+	for _, word := range []string{"Welcome", "to", "the", "Confluent", "Kafka", "Golang", "client"} {
+		p.Produce(&kafka.Message{
+			TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+			Value:          []byte(word),
+		}, nil)
+	}
+
+	// Wait for message deliveries before shutting down
+	p.Flush(15 * 1000)
+	return nil
+}
+
+func consume() error{
+	c, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers": "localhost:9092",
+		"group.id":          "myGroup",
+		"auto.offset.reset": "earliest",
+	})
+
+	if err != nil {
+		return err
+	}
+
+	c.SubscribeTopics([]string{"fooTopic"}, nil)
+
+	// A signal handler or similar could be used to set this to false to break the loop.
+	run := true
+
+	for run {
+		msg, err := c.ReadMessage(time.Second)
+		if err == nil {
+			fmt.Printf("Message on %s: %s\n", msg.TopicPartition, string(msg.Value))
+		} else if !err.(kafka.Error).IsTimeout() {
+			// The client will automatically try to recover from all errors.
+			// Timeout is not considered an error because it is raised by
+			// ReadMessage in absence of messages.
+			fmt.Printf("Consumer error: %v (%v)\n", err, msg)
+		}
+	}
+
+	return c.Close()
 }
